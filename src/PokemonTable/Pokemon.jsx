2b4cad570f411e83705aa98a9128c007723f1f78
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-01-14 10:56:24 +0200

    add PATCH to update hp

--- src/PokemonTable/Pokemon.jsx
+++ src/PokemonTable/Pokemon.jsx
@@ -1,5 +1,37 @@
 function Pokemon({ pokemon, fetchData }) {
- const deleteData = async (id) => {
+  
+  //GET POKEOM BY ID  ADD HP
+  const updateData = async (id) => {
+    const response = await fetch("http://localhost:3000/pokemon/" + id);
+    try{
+    if (!response.ok) {
+      throw new Error("Getting one pokemon was not successful");
+    }
+    const pokemon = await response.json();
+  
+    // UPDATE POKEMON HEALTH POINTS
+
+    const requestOptions = {
+      method: "PATCH",
+      header: {"Content-type": "application/json"},
+      body: JSON.stringify({hp: pokemon.hp + 10}),
+    };
+
+    const response2 = await fetch(
+      "http://localhost:3000/pokemon/" + id,
+      requestOptions
+    );
+     if (!response2.ok) {
+      throw new Error("Patch was not successful");
+    }
+    fetchData();
+  } catch (error){
+    alert(error.message);
+  }
+  };
+
+  // DELETE
+  const deleteData = async (id) => {
     try {
       const requestOptions = {
         method: "DELETE",
@@ -19,7 +51,7 @@ function Pokemon({ pokemon, fetchData }) {
       alert(error.message);
     }
   };
-
+  // ---------------------
 
   return (
     <div id="card" className="flex flex-col border p-30 mt-9 ">
@@ -33,6 +65,13 @@ function Pokemon({ pokemon, fetchData }) {
         className="bg-emerald-300 p-2"
       >
         Delete
+      </button>
+      <button
+        type="button"
+        onClick={() => updateData(pokemon.id)}
+        className="bg-emerald-300 p-2 relative top-1.5"
+      >
+        +10 Health points
       </button>
     </div>
   );
